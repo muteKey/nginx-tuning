ddb420e9ad816b7baff033a2d5bdfc764f9fd498
Kirill Ushkov <kirillushkov2010@gmail.com>
2021-11-25 03:02:51 +0200

    added caching possibility

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,13 +1,18 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+IMAGES_FOLDER = f"{os.getenv('APP_FOLDER')}/images"
 
 @app.route('/')
 def hello_world():
     return "<h1>Hello World!</h1>"
-    
+
+@app.route("/images/<path:filename>")
+def imagefiles(filename):
+    return send_from_directory(IMAGES_FOLDER, filename)
+
  
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
